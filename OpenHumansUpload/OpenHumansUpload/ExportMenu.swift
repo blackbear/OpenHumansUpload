d125f6a2fda844a485ef1baf25bda37d698223c0
James Turner <jturner@onlineconvergence.com>
2016-07-28 12:54:01 -0400

    Incremental uploads

--- OpenHumansUpload/OpenHumansUpload/ExportMenu.swift
+++ OpenHumansUpload/OpenHumansUpload/ExportMenu.swift
@@ -18,25 +18,23 @@ class ExportMenu: UIViewController {
     @IBOutlet weak var incrementalButton: UIButton!
     @IBOutlet weak var editUploadButton: UIButton!
     @IBOutlet weak var incrementalLabel: UILabel!
+    @IBOutlet weak var progressView1: UIProgressView!
     
     var alert : UIAlertController!
     var fileList : JSONArray = JSONArray()
+    var progressIncrement : Float!
     let healthStore     = HKHealthStore()
     
     func updateUI() {
-        let defs = NSUserDefaults.standardUserDefaults()
-        let lastDate = defs.objectForKey("lastSaveDate") as! NSDate?
-        if (lastDate == nil) {
-            incrementalButton.hidden = true
-            incrementalLabel.hidden = true
-        }
+        self.progressView.hidden = true
+        self.progressView1.hidden = true
+
         
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.uploadButton.layer.cornerRadius=5
-        self.incrementalButton.layer.cornerRadius=5
         self.updateUI()
     }
 
@@ -64,15 +62,17 @@ class ExportMenu: UIViewController {
     
         @IBOutlet weak var progressView: UIProgressView!
     
-    @IBAction func uploadIncremental(sender: AnyObject) {
-        self.view.userInteractionEnabled = false
-        activityIndicator.startAnimating()
-        let defs = NSUserDefaults.standardUserDefaults()
-        let lastDate = defs.objectForKey("lastSaveDate") as! NSDate?
-        let configuration   = HealthDataFullExportConfiguration(profileName: "Profilname", exportType: HealthDataToExportType.ALL, startDate: lastDate, endDate: NSDate())
+    func generateMonthData(now : NSDate, currentDate : NSDate) -> Void {
+        let cal = NSCalendar.currentCalendar()
+        var nextMonth = cal.dateByAddingUnit(NSCalendarUnit.Month, value: 1, toDate: currentDate, options: NSCalendarOptions(rawValue: 0))
+        nextMonth = cal.dateBySettingUnit(NSCalendarUnit.Day, value: 1, ofDate: nextMonth!, options: NSCalendarOptions(rawValue: 0))
+        var endOfMonth = cal.dateByAddingUnit(NSCalendarUnit.Second, value: -1, toDate: nextMonth!, options: NSCalendarOptions(rawValue: 0))
+        if (now.compare(endOfMonth!) == .OrderedAscending) {
+            endOfMonth = now
+        }
+        let configuration   = HealthDataFullExportConfiguration(profileName: "Profilname", exportType: HealthDataToExportType.ALL, startDate: currentDate, endDate: endOfMonth)
         let target = JsonSingleDocInMemExportTarget()
-        self.progressView.progress = 0
-        self.progressView.hidden = false
+        // create your instance of HKHeakthStore
         // and pass it to the HealthKitDataExporter
         let exporter        = HealthKitDataExporter(healthStore: healthStore)
         exporter.export(
@@ -83,7 +83,6 @@ class ExportMenu: UIViewController {
             
             onProgress: {
                 (message: String, progressInPercent: NSNumber?) -> Void in
-                // output progress messages
                 dispatch_async(dispatch_get_main_queue(), {
                     self.progressView.progress = (progressInPercent?.floatValue)!
                 })
@@ -92,131 +91,72 @@ class ExportMenu: UIViewController {
             onCompletion: {
                 (error: ErrorType?)-> Void in
                 // output the result - if error is nil. everything went well
-                if let exportError = error {
-                    print(exportError)
+                if error != nil {
+                    var continuing = false
+                    self.alert = UIAlertController(title: "Export Failed", message: error.debugDescription, preferredStyle: UIAlertControllerStyle.Alert)
+                    if error.debugDescription.containsString("Protected health data") {
+                        self.alert = UIAlertController(title: "Export Paused", message: "The export will pause if the phone is locked during the operation.", preferredStyle: UIAlertControllerStyle.Alert)
+                        continuing = true
+                        
+                    }
+                    self.alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler:  { (act) -> Void in
+                        if continuing {
+                            self.view.userInteractionEnabled = false
+                            self.activityIndicator.startAnimating()
+                        }
+                        }))
+                    
+                    self.presentViewController(self.alert, animated: true, completion: nil)
+                    return
                 } else {
                     let dayTimePeriodFormatter = NSDateFormatter()
                     dayTimePeriodFormatter.dateFormat = "yyyy-MM-dd"
-                    var fname = "healthkit-export_" + dayTimePeriodFormatter.stringFromDate(lastDate!)
-                    fname = fname + "_" + dayTimePeriodFormatter.stringFromDate(NSDate()) + "_"
-                    fname = fname + String(NSDate().timeIntervalSince1970) + ".json"
-                    print(fname + "\n")
+                    var fname = "healthkit-export_" + dayTimePeriodFormatter.stringFromDate(currentDate)
+                    fname = fname + "_" + dayTimePeriodFormatter.stringFromDate(endOfMonth!) + "_"
+                    fname = fname + String(now.timeIntervalSince1970) + ".json"
+                    if (target.hasSamples()) {
+                        OH_OAuth2.sharedInstance().uploadFile(fname, data: target.getJsonString(), memberId: OH_OAuth2.sharedInstance().memberId!, handler: { (success: Bool, filename: String?) -> Void in
+                            if success {
+                                dispatch_async(dispatch_get_main_queue(), {
+                                    self.progressView1.progress += self.progressIncrement
+                                });
+                                self.outputMonth(now, currentDate: nextMonth!)
+                                let defs = NSUserDefaults.standardUserDefaults()
+                                if endOfMonth?.compare(now) == .OrderedAscending {
+                                    defs.setObject(endOfMonth, forKey: "lastSaveDate")
+                                } else {
+                                    defs.setObject(now, forKey: "lastSaveDate")
+                                }
+                            } else {
+                                self.view.userInteractionEnabled = true
+                                self.activityIndicator.stopAnimating()
+                                self.alert = UIAlertController(title: "Upload Failed", message: "The upload failed, please try again later.", preferredStyle: UIAlertControllerStyle.Alert)
+                                self.alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+                                
+                                self.presentViewController(self.alert, animated: true, completion: nil)
+                                return
+                            }
+                            
+                            
+                        })
+                        
+                    } else {
+                        dispatch_async(dispatch_get_main_queue(), {
+                            self.progressView1.progress += self.progressIncrement
+                        });
+                        self.outputMonth(now, currentDate: nextMonth!)
+                        let defs = NSUserDefaults.standardUserDefaults()
+                        defs.setObject(endOfMonth, forKey: "lastSaveDate")
+                    }
                     
-                    OH_OAuth2.sharedInstance().uploadFile(fname, data: target.getJsonString(), memberId: OH_OAuth2.sharedInstance().memberId!, handler: { (success: Bool, filename: String?) -> Void in
-                        if success {
-                            dispatch_async(dispatch_get_main_queue(), {
-                                self.progressView.hidden = true
-                            })
-                            
-                            OH_OAuth2.sharedInstance().getMemberInfo({ (memberId, messagePermission, usernameShared, username, files) in
-                                self.fileList = files
-                                dispatch_async(dispatch_get_main_queue(),{
-                                    defs.setObject(NSDate(), forKey: "lastSaveDate")
-
-                                        self.updateUI()
-                                        self.activityIndicator.stopAnimating()
-                                    self.view.userInteractionEnabled = true
-                                    self.alert = UIAlertController(title: "Upload Succeeded", message: "Your healthkit data has been uploaded to OpenHumans.", preferredStyle: UIAlertControllerStyle.Alert)
-                                    self.alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
-                                    
-                                    self.presentViewController(self.alert, animated: true, completion: nil)
-                                })
-                                }, onFailure: {
-                                    self.activityIndicator.stopAnimating()
-                                    self.view.userInteractionEnabled = true
-                                    
-                                    
-                            })
-                            
-                        } else {
-                            self.view.userInteractionEnabled = true
-                            self.activityIndicator.stopAnimating()
-                            self.alert = UIAlertController(title: "Upload Failed", message: "The upload failed, please try again later.", preferredStyle: UIAlertControllerStyle.Alert)
-                            self.alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
-                            
-                            self.presentViewController(self.alert, animated: true, completion: nil)
-                            
-                            
-                        }
-                    })
                 }
-            }
-        )
+        })
+        
     }
     
     func outputMonth(now : NSDate, currentDate : NSDate) -> Void {
         if currentDate.compare(now) != NSComparisonResult.OrderedDescending {
-            let cal = NSCalendar.currentCalendar()
-            let nextMonth = cal.dateByAddingUnit(NSCalendarUnit.Month, value: 1, toDate: currentDate, options: NSCalendarOptions(rawValue: 0))
-            let endOfMonth = cal.dateByAddingUnit(NSCalendarUnit.Second, value: -1, toDate: nextMonth!, options: NSCalendarOptions(rawValue: 0))
-            let configuration   = HealthDataFullExportConfiguration(profileName: "Profilname", exportType: HealthDataToExportType.ALL, startDate: currentDate, endDate: endOfMonth)
-            let target = JsonSingleDocInMemExportTarget()
-            // create your instance of HKHeakthStore
-            // and pass it to the HealthKitDataExporter
-            let exporter        = HealthKitDataExporter(healthStore: healthStore)
-            exporter.export(
-                
-                exportTargets: [target],
-                
-                exportConfiguration: configuration,
-                
-                onProgress: {
-                    (message: String, progressInPercent: NSNumber?) -> Void in
-                    // output progress messages
-                    dispatch_async(dispatch_get_main_queue(), {
-                        //                        print(message)
-                    })
-                },
-                
-                onCompletion: {
-                    (error: ErrorType?)-> Void in
-                    // output the result - if error is nil. everything went well
-                    if let exportError = error {
-                        print(exportError)
-                    } else {
-                        let dayTimePeriodFormatter = NSDateFormatter()
-                        dayTimePeriodFormatter.dateFormat = "yyyy-MM-dd"
-                        var fname = "healthkit-export_" + dayTimePeriodFormatter.stringFromDate(currentDate)
-                        fname = fname + "_" + dayTimePeriodFormatter.stringFromDate(endOfMonth!) + "_"
-                        fname = fname + String(now.timeIntervalSince1970) + ".json"
-                        if (target.hasSamples()) {
-                            print(fname + "\n")
-                            print(target.getJsonString())
-                            OH_OAuth2.sharedInstance().uploadFile(fname, data: target.getJsonString(), memberId: OH_OAuth2.sharedInstance().memberId!, handler: { (success: Bool, filename: String?) -> Void in
-                                if success {
-                                    self.outputMonth(now, currentDate: nextMonth!)
-                                    let defs = NSUserDefaults.standardUserDefaults()
-                                    if endOfMonth?.compare(now) == .OrderedAscending {
-                                        defs.setObject(endOfMonth, forKey: "lastSaveDate")
-                                    } else {
-                                        defs.setObject(now, forKey: "lastSaveDate")
-                                    }
-                                } else {
-                                    self.view.userInteractionEnabled = true
-                                    self.activityIndicator.stopAnimating()
-                                    self.alert = UIAlertController(title: "Upload Failed", message: "The upload failed, please try again later.", preferredStyle: UIAlertControllerStyle.Alert)
-                                    self.alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
-                                    
-                                    self.presentViewController(self.alert, animated: true, completion: nil)
-                                    return
-                                }
-                                
-                                
-                            })
-                            
-                        } else {
-                            print("No data for " + dayTimePeriodFormatter.stringFromDate(currentDate))
-                            self.outputMonth(now, currentDate: nextMonth!)
-                            let defs = NSUserDefaults.standardUserDefaults()
-                            if endOfMonth?.compare(now) == .OrderedAscending {
-                                defs.setObject(endOfMonth, forKey: "lastSaveDate")
-                            } else {
-                                defs.setObject(now, forKey: "lastSaveDate")
-                            }
-                        }
-
-                    }
-            })
+            self.generateMonthData(now, currentDate: currentDate)
         } else {
             
             OH_OAuth2.sharedInstance().getMemberInfo({ (memberId, messagePermission, usernameShared, username, files) in
@@ -226,13 +166,13 @@ class ExportMenu: UIViewController {
                     self.view.userInteractionEnabled = true
                     self.alert = UIAlertController(title: "Upload Succeeded", message: "Your healthkit data has been uploaded to OpenHumans.", preferredStyle: UIAlertControllerStyle.Alert)
                     self.alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
-                    
                     self.presentViewController(self.alert, animated: true, completion: nil)
+                    self.updateUI()
                 })
                 }, onFailure: {
                     self.activityIndicator.stopAnimating()
                     self.view.userInteractionEnabled = true
-                    
+                    self.updateUI()
                     
             })
             
@@ -243,12 +183,26 @@ class ExportMenu: UIViewController {
     @IBOutlet weak var datePickView: UIView!
     @IBOutlet weak var startButton: UIButton!
     @IBAction func uploadData(sender: AnyObject) {
-        view.userInteractionEnabled = false
-        activityIndicator.startAnimating()
         let defs = NSUserDefaults.standardUserDefaults()
+        var lastDate = defs.objectForKey("lastSaveDate")
+        if lastDate == nil {
+            lastDate = NSDate(timeIntervalSince1970: 1104537600);
+        }
+        view.userInteractionEnabled = false
+        self.progressView1.progress = 0.0
+        self.progressView.progress = 0.0
+        self.progressView.hidden = false
+        self.progressView1.hidden = false
+        activityIndicator.startAnimating()
         let now = NSDate()
-        defs.setObject(now, forKey: "lastSaveDate")
-        outputMonth(now, currentDate: NSDate(timeIntervalSince1970: 1104537600))
+        let cal = NSCalendar.currentCalendar()
+        let nmonths = cal.components(.Month, fromDate: lastDate as! NSDate, toDate: now, options: NSCalendarOptions.MatchFirst).month
+        if (nmonths == 0) {
+            progressIncrement = 1.0
+        } else {
+            progressIncrement = 1.0 / Float(nmonths)
+        }
+        outputMonth(now, currentDate: lastDate as! NSDate)
     }
 
 
--- OpenHumansUpload/OpenHumansUpload/ViewController.swift
+++ OpenHumansUpload/OpenHumansUpload/ViewController.swift
@@ -32,6 +32,9 @@ class ViewController: UIViewController {
         actionButton.hidden = true
         let oauth = OH_OAuth2.sharedInstance();
         if oauth.hasCachedToken() {
+            instructions.text = "Logging in..."
+            instructions.hidden = false
+           
             OH_OAuth2.sharedInstance().authenticateOAuth2(self, allowLogin: false, handler: { (status : AuthorizationStatus) -> Void in
                 if (status == AuthorizationStatus.AUTHORIZED) {
                     OH_OAuth2.sharedInstance().getMemberInfo({ (memberId, messagePermission, usernameShared, username, files) in
@@ -65,7 +68,7 @@ class ViewController: UIViewController {
     
     
     func authenticateRequiresLogin() {
-        instructions.text = "Welcome to the Open Humans HealthKit Uploader. This tool will allow you to upload your data in JSON format to the Open Humans website so that researchers can access the information you have recorded on your device.\n\nTo begin, you need to authenticate your identity with the Open Humans Web Site."
+        instructions.text = "Welcome to the Open Humans HealthKit Uploader. This tool will allow you to upload your HealthKit data (visible in the Health app) to your Open Humans account so that researchers can access the information you have recorded on your device.\n\nTo begin, you need to authenticate your identity with the Open Humans Web Site."
         actionButton.setTitle("Go to Open Humans website", forState: .Normal)
         instructions.hidden = false
         actionButton.hidden = false
